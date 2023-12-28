ce30cef7666c4c40fc28663fc68f86a0e8afc46e
Ryan Yin <xiaoyin_c@qq.com>
2023-12-29 01:24:03 +0800

    fix: (value "Unbound variable: ~S") (value (linux-libre-6.6))

--- README.md
+++ README.md
@@ -21,3 +21,7 @@ Tutorials for Guix itself:
 - [GNU Guix Reference Manual](https://guix.gnu.org/en/manual/en/guix.html): read this first for installation and basic usage & setup.
 - [GNU Guix Cookbook](https://guix.gnu.org/en/cookbook/en/guix-cookbook.html): read this after you have your Guix installed and have some basic knowledge about Guix.
 
+## Notes
+
+1. `cons* arg1 arg2 …`: a syntax sugar of `(cons arg1 (cons arg2 (cons … argn)))`
+
--- channels.scm
+++ channels.scm
@@ -1,18 +1,12 @@
-(list
-  ;; packages some non-free software which cannot be included in the official distribution
-  (channel
-    (name 'nonguix)
-    (url "https://github.com/nonguix/nonguix")
-    ;; Enable signature verification:
-    (introduction
-     (make-channel-introduction
-      "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-      (openpgp-fingerprint
-       "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-  ;; default guix channel
-  ;; https://git.savannah.gnu.org/cgit/guix.git/refs/heads
-  (channel
-    (inherit (car %default-channels))
-    ;; Stay on the version 1.4.0 to avoid too much update
-    (branch "version-1.4.0")))
-
+(cons* (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        ;; Enable signature verification:
+        (introduction
+         (make-channel-introduction
+          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+          (openpgp-fingerprint
+           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+       ;; default guix channel
+       ;; https://git.savannah.gnu.org/cgit/guix.git/refs/heads
+       %default-channels)
